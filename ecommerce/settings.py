2d5eda4a181a352f3bec1dd2bfed73d5d3e45645
blckops0517 <keerthigro321@gmail.com>
2025-10-27 15:52:01 +0530

    now

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    "ecommerce/static",
+    BASE_DIR / 'ecommerce' /'static',
 ]
 
 # meadia files configuration
